1728e783e995fb00520da2fc2540df269cb2d2fc
Reema <Reemaalshohof88@gmail.com>
2021-09-30 00:27:54 +0200

    delete render html pages and response with json in order to test the app with super test

--- homework/hackyourtemperature/__tests__/app.test.js
+++ homework/hackyourtemperature/__tests__/app.test.js
@@ -1,20 +1,48 @@
+import { expect } from "@jest/globals";
 import supertest from "supertest";
 import app from "../app.js";
 
 const request = supertest(app);
-
+//happy path
 describe("POST /", () => {
   it("should response with a 200 status code ", async () => {
-    const response = await request
-      .post("/weather")
-      .field("cityName", "Amsterdam");
-    expect(response.status).toBe(200);
+    const response = await request.post("/amsterdam").then((response) => {
+      expect(response.status).toBe(200);
+    });
   });
 
-  // it("should return the city name in the response", async () => {
-  //   const response = await request
-  //     .post("/weather")
-  //     .field("cityName", "Amsterdam");
-  //   expect(response.body).to.stringContaining("Amsterdam");
-  // });
+  it("should return the city name in the response", async () => {
+    const response = await request
+      .post("/amsterdam")
+
+      .then((response) => {
+        expect(response.body).toHaveProperty("cityName", "Amsterdam");
+      });
+  });
+  it("should return temperature in the response", async () => {
+    const response = await request
+      .post("/amsterdam")
+
+      .then((response) => {
+        expect(response.body).toHaveProperty("temperature");
+      });
+  });
+});
+
+//unhappy path
+describe("POST /", () => {
+  it("should response with a 204 status code when the city not found ", async () => {
+    const response = await request.post("/42").then((response) => {
+      expect(response.status).toBe(404);
+    });
+  });
+
+  it("should return city not found", async () => {
+    const response = await request
+      .post("/42")
+
+      .then((response) => {
+        expect(response.body).toHaveProperty("weatherText", "City not found!");
+      });
+  });
 });
--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -22,28 +22,31 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 //
 app.use(express.json());
 
-//routing
+//routing;
 app.get("/weather", (req, res) => {
   res.render("weather", { title: "Weather Data" });
 });
 //handle post request from the client
-app.post("/weather", urlencodedParser, async (req, res) => {
-  const cityName = req.body.cityName;
+app.post("/:cityName", async (req, res) => {
+  const cityNameInput = req.params.cityName;
   try {
     const endpoint = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityNameInput}&appid=${API_KEY}&units=metric`
     );
     if (endpoint.status === 200) {
-      const cityTemperature = await endpoint.json();
-      const temperature = cityTemperature.main.temp;
-      res.render("weather", {
-        weatherText: `The temperature in ${cityName} is ${temperature} °C!`,
+      const cityWeatherInfo = await endpoint.json();
+      const cityTemperature = cityWeatherInfo.main.temp;
+      res.json({
+        cityName: cityWeatherInfo.name,
+        temperature: cityTemperature,
       });
     } else {
-      res.render("weather", { weatherText: "City not found!" });
+      res.status(404);
+      res.json({ weatherText: "City not found!" });
     }
   } catch (error) {
     console.log(error.message);
   }
 });
+
 export default app;
